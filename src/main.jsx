ae180aa8d69ee2d1bcb9877484991c8b455d70d4
Praise-Tech-Dev <spraiseadeogun@gmail.com>
2025-04-08 20:35:12 +1000

    my change

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,10 @@ import './index.css'
 import App from './App.jsx'
 
 import 'primeicons/primeicons.css';
-import Router from './route/router.jsx';
+import Router from './route/Router.jsx';
 
 
-
+// l;khu8njybgybg
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Router />
